d57d950573bba2019598c3b77d5121c7a0fcd738
Carter Reed Hranac <crh244@gmail.com>
2017-10-18 23:40:10 +1300

    added batchScript_2

---
+++ R/batchScript_2.R
@@ -0,0 +1,8 @@
+source("R/helperFunctions.R")
+classMod("ptr", run.id = "DBL_2", nrep = 10, dbl = T)
+classMod("mic", run.id = "DBL_2", nrep = 10, dbl = T)
+classMod("mol", run.id = "DBL_2", nrep = 10, dbl = T)
+
+classMod("ptr", run.id = "SNG_2", nrep = 10, dbl = F)
+classMod("mic", run.id = "SNG_2", nrep = 10, dbl = F)
+classMod("mol", run.id = "SNG_2", nrep = 10, dbl = F)
