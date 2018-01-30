47dab0c8ec8a3c3f5d75e8a9ad7acf9bc6ea0297
Carter Reed Hranac <crh244@gmail.com>
2018-01-30 23:22:10 +1300

    plot work
    sorted the silhouttes, working to complete fig1 pannel a

--- R/pubPlots.R
+++ R/pubPlots.R
@@ -3,7 +3,7 @@
 ###############################################################################
 source("R/helperFunctions.R")
 
-library(ggplot2); library(dplyr)
+library(ggplot2); library(dplyr); library(data.table)
 #### Figure 1 ####
 #3 pannel figure, 1 pannel, map w/ outbreak locations (symbols for an/hum), 
 # and 2 outbreak time lines, 1 entire history, one collapsed year
@@ -11,7 +11,7 @@ library(ggplot2); library(dplyr)
 an.ob <- fread(file.path(clean.dir, "annOB.PPM.csv")) #animal points
 an.an <- fread(file.path(data.source, "Animal_Index_Jan2018.csv")) #notes on outbreaks
 an.sub <- an.an %>%
-  dplyr::select(OUTBREAK_ID, Org_smp, Year_Start, Month_Start)
+  dplyr::select(OUTBREAK_ID, Org_smp, Year_Start)
 names(an.sub)[1] <- names(an.ob)[1]
 an.full <- an.sub %>%
   left_join(an.ob,an.sub, by = "Outbreak_ID")
@@ -19,14 +19,14 @@ an.full <- an.sub %>%
 hum.ob <- fread(file.path(clean.dir, "humOB.PPM.csv"))
 hum.an <- fread(file.path(data.source, "Human_Index_30May.csv"))
 hum.sub <- hum.an %>%
-  dplyr::select(Outbreak_ID, Month_Start, Year_Start)
+  dplyr::select(Outbreak_ID, Year_Start)
 hum.full <- hum.sub %>%
   left_join(hum.ob, hum.sub, by = "Outbreak_ID")
 
 
 hum.full$Org_smp <- "human"
 hum.s <- hum.full %>%
-  dplyr::select(names(an.full))
+  dplyr::select(which(names(hum.full) %in% names(an.full)))
 
 ob.full <- bind_rows(hum.s, an.full)
 colnames(ob.full)[3:4] <- c( "long", "lat")
@@ -37,6 +37,9 @@ for(i in 1:nrow(ob.full)){
   ifelse(ob.full[i,"Org_smp"] == "human", z[[i]] <- "human",z[[i]] < "animal")  
 }
 
+# library(devtools)
+# devtools::install_github("sckott/rphylopic")
+library(rphylopic)
 ## name id ##
 bat <- 	104257
 chimpanzee <- 109082
@@ -44,20 +47,37 @@ duiker <-  2478895
 gorilla <-  763767
 human <- 	109086
 
-library(rphylopic)
 critters <- c(bat, chimpanzee, gorilla, duiker, human)
 critters.id <- lapply(critters, ubio_get)
-name_images
-image_data()[[1]]
+critter.names <- lapply(critters.id, name_images) ## Needs manual attention
+c.names <- c(critter.names[[1]]$same[[1]]$uid, 
+             critter.names[[2]]$same[[1]]$uid,
+             critter.names[[3]]$same[[1]]$uid,
+             critter.names[[4]]$supertaxa[[1]]$uid,
+             critter.names[[5]]$same[[1]]$uid)
+critter.img <- lapply(c.names, image_data, size = 64)
+c.img <- list()
+for(i in 1:length(c.names)){
+  c.img[[i]] <- critter.img[[i]][[1]]
+}
 
+## create vector of images
+img.list <- list()
+for(i in 1:nrow(ob.full)){
+  j <- match(ob.full[i,"Org_smp"], levels(ob.full$Org_smp)) 
+  img.list[[i]] <- c.img[[j]]
+}
 
-
-
-bat.img <- image_get(critters.id[[5]][[1]], size = 64)[[1]]
-
+## create color vector
+cz <- c("darkorange2", "black", "green3", "dodgerblue2", "magenta1")
+col.list <- list()
+for(i in 1:nrow(ob.full)){
+  j <- match(ob.full[i,"Org_smp"], levels(ob.full$Org_smp)) 
+  col.list[[i]] <- cz[[j]]
+}
 
 #### Pannel 1 Map ####
-library(rgdal)
+library(rgdal);library(rgeos);library(raster)
 ## accessory layers
 afr.poly <- readOGR(dsn = file.path(data.source, "Africa"),
                     layer = "AfricanCountires")
@@ -85,11 +105,24 @@ ob.plot <- ggplot() +
                colour = "white", 
                alpha = .25,
                fill = "cornsilk") +
-  geom_point(data = fortify(ob.full),
-             aes(x = long, y = lat,
-                 group = Org_smp,
-                 shape = Org_smp,
-                 colour = Org_smp)) +
-  scale_shape_manual(values = c(15:19))+
-  add_phylopic(img)
+  coord_cartesian(xlim = c(-20, 53),
+                  ylim = c(-36, 40))
+  
+  
+for( i in 1:nrow(ob.full)){
+  ob.plot <- ob.plot + add_phylopic(img.list[[i]], 1, 
+                                    ob.full$long[i],
+                                    ob.full$lat[i],
+                                    ysize = 2.5,
+                                    color = col.list[[i]])
+}
+  
+  
 ob.plot + bkg
+# geom_point(data = fortify(ob.full),
+#            aes(x = long, y = lat,
+#                group = Org_smp,
+#                shape = Org_smp,
+#                colour = Org_smp)) +
+# scale_shape_manual(values = c(15:19))+
+# add_phylopic(c.img[[3]])
