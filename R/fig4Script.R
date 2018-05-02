f0c9df0b331cd8aed27426953edcd1eccaffc588
Jonathan Marshall <jmarshall@never.you.mind>
2018-05-02 13:00:06 +1200

    update ERridge to make it a bit prettier and easier to align

--- R/fig4Script.R
+++ R/fig4Script.R
@@ -8,7 +8,8 @@
 
 source("R/helperFunctions.R")
 library(ggplot2); library(dplyr); library(data.table); library(gridExtra); library(gtools)
-library(rgdal); library(raster); library(ggridges)
+library(rgdal); library(raster); library(ggridges); library(RcppRoll)
+
 #### functions ####
 spatHandler <- function(model.string){
   ## function for loading rasters produced from spatGLM and producing an averaged product based on the
@@ -76,43 +77,44 @@ ERgplot <- function(x, source.path = data.source, afr = afr.poly, rf = rf.poly){
   out <- g.plot + theme_bw()
 }
 
-ERridge <- function(x, n.bin, crop.extent = sub.ext){
+ERridge <- function(x, n.bin, scale = 5, crop.extent = sub.ext){
   ## Function for creating ridgeline density plots of the breeding force
   ## used on objects creaded from sumGen (since it loads rasterlayers as well)
   x.crop <- raster::crop(x[[1]],y = raster::extent(crop.extent))  
   x.cv <- as.data.frame(rasterToPoints(x.crop))
-  colnames(x.cv)[3:ncol(x.cv)] <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+  # convert to a data frame and add in a half month at average of Jan+Dec at either end
+  colnames(x.cv)[3:ncol(x.cv)] <- 1:12
   x.df <- x.cv[complete.cases(x.cv),]
-  x.df$Jan2 <- x.df$Jan
+  x.df$`0.5` <- x.df$`12.5` <- (x.df$`1` + x.df$`12`)/2
+  # gather everything together and compute averages per month across the latitude breaks
   ER.df <- x.df %>%
-    tidyr::gather("month","ER",3:ncol(x.df)) %>%
+    tidyr::gather("month","ER",3:ncol(x.df), convert=TRUE) %>%
     mutate(strata = cut(y, breaks = n.bin)) %>%
     group_by(strata, month) %>%
     summarise(ER.mean = mean(ER))
-  
-  ER.df$month <-   factor(ER.df$month,levels=c("Jan","Feb","Mar",
-                                               "Apr","May","Jun",
-                                               "Jul","Aug","Sep",
-                                               "Oct","Nov","Dec", "Jan2"))
-  
-  ER.ridge <- ggplot(data= ER.df,
-                     aes(x= month,y= strata,height = ER.mean, group = strata, fill = ER.mean))+
-    geom_density_ridges_gradient(stat = "identity", scale = 5) +
+
+  # rolling average for fill colour
+  ER.df2 <- ER.df %>% group_by(strata) %>% arrange(month) %>% mutate(Roll.mean = roll_mean(ER.mean, n=2, fill=0))
+
+  # plot
+  ER.ridge <- ggplot(data= ER.df2,
+         aes(x= month,y= strata,height = ER.mean, group = strata, fill = Roll.mean)) +
+    geom_density_ridges_gradient(color="#0000000F", stat = "identity", scale = scale) +
     scale_fill_gradient(low= "yellow", high = "red4",
                         limits = c(0,max(ER.df$ER.mean)),
                         name = "Mean \nEbola \nRisk") +
-    scale_x_discrete(label = c("Jan","Feb","Mar",
-                               "Apr","May","Jun",
-                               "Jul","Aug","Sep",
-                               "Oct","Nov","Dec", "Jan"),
-                     expand = c(0,0))+
+    scale_x_continuous(breaks = 1:12, labels=month.abb[1:12],
+                       expand = c(0,0))+
+    scale_y_discrete(expand=c(0,0)) +
     theme_bw() +
     theme(
       axis.title = element_blank(),
       axis.text.y = element_blank(),
-      axis.ticks = element_blank()
+      axis.ticks.y = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.y = element_blank(),
+      panel.grid.major.x = element_line(colour="grey96")
     )
-  
   return(ER.ridge)
 }
 
