bc9835097001961c424a18118180f7d9356f3240
Carter Reed Hranac <crh244@gmail.com>
2019-05-02 09:14:29 +1200

    Trouble shooting: ViralSmash
    Created the correct outbreak rasters including the 34 and 32 point versions of human outbreaks.

--- munge/ViralSmash.R
+++ munge/ViralSmash.R
@@ -61,6 +61,7 @@ viralRasterGen <- function(df, name, mask, out.dir = NULL){
   }
   out.stk <- do.call(stack, out)
   if(!is.null(out.dir)){
+    ## Change here for names
     writeRaster(out.stk, file.path(out.dir, "OB_2"), format = "GTiff",
                 bylayer = T, suffix = "names", overwrite = T)
   }
@@ -84,6 +85,7 @@ viralRasterGen0 <- function(df, name, z.mask, out.dir = NULL){
   }
   out.stk <- do.call(stack, out)
   if(!is.null(out.dir)){
+    ## Change here for names
     writeRaster(out.stk, file.path(out.dir, "OB_2"), format = "GTiff",
                 bylayer = T, suffix = "names", overwrite = T)
   }
@@ -101,7 +103,7 @@ wgs <- proj4string(c.mask)
 ## UPDATE 12.2.018: additional entry for 2017 DRC outbreak
 ## UPDATE 28/08/2018 updated path to below which is kept up to date with all outbreaks.
   ## points are all within this database so need for seperate poly and point methods
-hum.src <- read.csv(file.path("data/HumOutbreakDB_2.csv"))
+
 # hum.simple <- hum.src %>% dplyr::select(Outbreak_ID, Virus, Month.Start, Month.Start, Month.End , Class)
   
 #   #### Polygons ####
@@ -138,7 +140,19 @@ hum.src <- read.csv(file.path("data/HumOutbreakDB_2.csv"))
 # names(hu.ma) <- "Outbreak_ID"
 # 
 # ma.hu <- inner_join(as.data.frame(hu.ma), hum.simple, by = "Outbreak_ID")
+
   #### OUT ####
+hum.src <- read.csv(file.path("data/HumOutbreakDB.csv"))
+humOB.bi <- biGenV(hum.src)
+write.csv(humOB.bi, file.path(clean.dir.nov,"humOB.PPM.csv"), row.names = F)
+humOB.occ <- occGenV(humOB.bi, "hum", c.mask)
+write.csv(humOB.occ, file.path(clean.dir.nov,"humOcc.csv"), row.names = F)
+viralRasterGen(humOB.bi, "hum", c.mask, file.path(clean.dir.nov))
+viralRasterGen0(humOB.bi, "hum", z.mask, file.path(clean.dir.nov))
+
+## 34 Point data
+# Note: must change internal viralRasterGen functions for naming
+hum.src <- read.csv(file.path("data/HumOutbreakDB_2.csv"))
 humOB.bi <- biGenV(hum.src)
 write.csv(humOB.bi, file.path(clean.dir.nov,"humOB.PPM_2.csv"), row.names = F)
 humOB.occ <- occGenV(humOB.bi, "hum", c.mask)
